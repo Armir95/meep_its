112334e413587f829a22c18e8bb6a7d31a0453f4
Armir Trupja <tarmir95@gmail.com>
2023-09-17 23:27:47 +0200

    versione finale aggiornata

--- the-senseis/MEEP.Client/sign-in/media/ictsbackend-the-senseis-b0046b060403/MEEP.Client/profilo/script/script.js
+++
@@ -1,9 +0,0 @@
-// Funzione per attivare/disattivare il dropdown menu
-function toggleDropdown() {
-    var dropdown = document.getElementById("myDropdown");
-    if (dropdown.style.display === "none") {
-        dropdown.style.display = "block";
-    } else {
-        dropdown.style.display = "none";
-    }
-}
--- the-senseis/MEEP/src/main/java/com/thesensei/MEEP/repository/AccompagnatoreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.thesensei.MEEP.repository;
-
-import com.thesensei.MEEP.entity.Accompagnatore;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccompagnatoreRepository extends JpaRepository<Accompagnatore, Integer> {
-}
